3202d12a8d7d07bb710f28f158ebb7dd989a3fab
めがねこ <29650686+MeGaNeKoS@users.noreply.github.com>
2022-06-28 08:57:44 +0700

    Update code.js

--- code.js
+++ code.js
@@ -7,28 +7,39 @@ if (WebSocket.prototype.original == undefined) {
 }
 
 let last_channel = null
+
+let mode = 0
+
 WebSocket.prototype.send = function(data) {
     if (Object.prototype.toString.call(data) === "[object ArrayBuffer]") {
-        if (decoder.decode(data).includes("self_mutes\u0004true") || decoder.decode(data).includes("self_deafs\u0004true")) {
+		if (decoder.decode(data).includes("guild_ids\u0003nil")) {
+                // in here we are disconnect from vc
+                last_channel = null
+		}
+		
+		if (decoder.decode(data).includes(last_channel)) {
+			if (mode === 0 ){
+				// allowing deafen/mute
+				if (decoder.decode(data).includes("self_mutes\u0005false") && decoder.decode(data).includes("self_deafs\u0005false")) {
+					return
+				}
+			} else if (mode === 1){
+				// deafen only
+				if (decoder.decode(data).includes("self_deafs\u0005false")) {
+					return
+				}
+			} else if (mode === 2){
+				// normal
+			}
+		} 
+		if (decoder.decode(data).includes("channel_idm\u0000\u0000\u0000")) {
+			// we detect a voice channel in the data
             // store the new channel
             start = decoder.decode(data).split("channel_idm\u0000\u0000\u0000")[1]
             end = start.split("\u0000\u0000\u0000	self_mutes")[0]
             last_channel = "channel_idm\u0000\u0000\u0000" + end + "\u0000\u0000\u0000	self_mutes"
-        } else if (decoder.decode(data).includes("self_mutes\u0005false") || decoder.decode(data).includes("self_deafs\u0005false")) {
-
-            if (decoder.decode(data).includes("guild_ids\u0003nil")) {
-                // in here we are disconnect from vc
-                last_channel = null
-            } else if (decoder.decode(data).includes(last_channel)) {
-                // something want to update our deafen/mute status
-                return
-            } else {
-                // store the new channel
-                start = decoder.decode(data).split("channel_idm\u0000\u0000\u0000")[1]
-                end = start.split("\u0000\u0000\u0000	self_mutes")[0]
-                last_channel = "channel_idm\u0000\u0000\u0000" + end + "\u0000\u0000\u0000	self_mutes"
-            }
-        }
+        } 
+        
     }
     WebSocket.prototype.original.apply(this, [data]);
 }
